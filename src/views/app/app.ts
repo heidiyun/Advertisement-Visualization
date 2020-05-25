72c233b7f052464ed2236cdca94718584f3e41cf
Yunjeong Goo <heidiyun.goo@gmail.com>
2020-05-25 20:50:52 +0900

    init

---
+++ src/views/app/app.ts
@@ -0,0 +1,7 @@
+import { Vue, Component } from "vue-property-decorator";
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+
+Vue.use(Antd);
+@Component({})
+export default class App extends Vue {}
---
+++ src/views/app/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script src="./app.ts"></script>
+
+<style lang="scss">
+@import "./app.scss";
+</style>
