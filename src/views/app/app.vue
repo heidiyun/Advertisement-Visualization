25e3a40c8e4809bd28efad5ec72361673a864a92
sooojungee <sooojungee@gmail.com>
2020-06-05 23:58:09 +0900

    test

--- src/views/app/app.vue
+++ src/views/app/app.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <v-layout style="flex-wrap:wrap">
-      <v-flex style="border:1px solid; height:400px;" md6>
+      <v-flex style="border:1px solid; height:300px;" md6>
         <scatter-plot></scatter-plot>
       </v-flex>
-      <v-flex style="border:1px solid; height:400px;" md6>
+      <v-flex style="border:1px solid; height:300px;" md6>
         <line-graph></line-graph>
       </v-flex>
-      <v-flex style="border:1px solid; height:400px;" md6>
+      <v-flex style="border:1px solid; height:300px;" md6>
         <table></table>
       </v-flex>
-      <v-flex style="border:1px solid; height:400px;" md6></v-flex>
+      <v-flex style="border:1px solid; height:300px;" md6></v-flex>
     </v-layout>
     <router-view></router-view>
   </div>
