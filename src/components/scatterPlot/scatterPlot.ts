b53506ce121ec16371ec7e5f8c34d62eabc2ea1a
JunyupHong <junyup.hong@gmail.com>
2020-06-11 18:21:14 +0900

    scatter plot - metric2 변경시 데이터 변경, adset 별 color 지정

--- src/components/scatterPlot/scatterPlot.ts
+++ src/components/scatterPlot/scatterPlot.ts
@@ -1,6 +1,11 @@
-import { Vue, Component } from "vue-property-decorator";
+import { Vue, Component, Watch } from "vue-property-decorator";
 import * as d3 from "d3";
 
+interface TAdset {
+  x: number;
+  y: number;
+  colorHSL: { h: number; s: number; l: number };
+}
 @Component({})
 export default class ScatterPlot extends Vue {
   // TODO
@@ -9,27 +14,127 @@ export default class ScatterPlot extends Vue {
   // data가 변경될 때 this.svg 다시 렌더링
   // 2. color hsl 로 적용
 
-  private data: Array<{ x: number; y: number }> = [];
+  private data: Array<tAdset> = [];
+  private selectedMetric: { 1?: string; 2?: string } = {
+    1: "metric1",
+    2: "metric2"
+  };
 
   private margin: {
     top: number;
     right: number;
     bottom: number;
     left: number;
-  } = { top: 20, right: 100, bottom: 30, left: 40 };
-  private width: number = 820 - this.margin.left - this.margin.right;
+  } = { top: 20, right: 10, bottom: 30, left: 40 };
+  private width: number = 820 - 120 - this.margin.left - this.margin.right;
   private height: number = 360 - this.margin.top - this.margin.bottom;
 
   private svg: any;
-  private x: any;
-  private y: any;
   private tooltip: any;
+  private xAxis: any;
+  private yAxis: any;
+  private dots: any;
 
-  private mounted() {
-    for (let i = 0; i < 100; i++) {
-      this.data.push({ x: Math.random() * 100, y: Math.random() * 100 });
+  @Watch("selectedMetric", { deep: true })
+  onMetricChange(newVal: { 1?: string; 2?: string }) {
+    console.log("[ON CHANGE METRIC]", newVal);
+    const temp = [];
+    const adsetCount = 2;
+    const dotCountPerAdset = 20;
+    if (newVal[1] && newVal[2]) {
+      for (let i = 0; i < adsetCount; i++) {
+        const c = Math.random() * 360;
+        for (let j = 1; j <= dotCountPerAdset; j++) {
+          temp.push({
+            x: Math.random() * 100,
+            y: Math.random() * 100,
+            colorHSL: {
+              h: c,
+              s: (j + 0.5) / dotCountPerAdset,
+              l: 0.5
+            }
+          });
+        }
+      }
     }
 
+    this.data = temp;
+    this.renderScatterPlot();
+  }
+
+  private changeMetric2(value: string) {
+    this.selectedMetric[2] = value;
+    console.log("[CHANGE METRIC 2]", this.selectedMetric[2]);
+  }
+
+  private initScatterPlot() {
+    // x축 그리기
+    this.xAxis = d3
+      .scaleLinear()
+      .domain([0, 0])
+      .range([0, this.width]);
+    this.svg
+      .append("g")
+      .attr("class", "xAxis")
+      .attr("transform", "translate(0, " + this.height + ")")
+      .style("color", "#888")
+      .call(d3.axisBottom(this.xAxis));
+
+    // y축 그리기
+    this.yAxis = d3
+      .scaleLinear()
+      .domain([0, 0])
+      .range([this.height, 0]);
+    this.svg
+      .append("g")
+      .attr("class", "yAxis")
+      .style("color", "#888")
+      .call(d3.axisLeft(this.yAxis));
+  }
+
+  private renderScatterPlot() {
+    // x 축 update
+    this.xAxis = d3
+      .scaleLinear()
+      .domain([0, Math.max(...this.data.map(d => d.x), 0)])
+      .range([0, this.width]);
+    this.svg.selectAll("g.xAxis").call(d3.axisBottom(this.xAxis));
+
+    // y축 update
+    this.yAxis = d3
+      .scaleLinear()
+      .domain([0, Math.max(...this.data.map(d => d.y), 0)])
+      .range([this.height, 0]);
+    this.svg.selectAll("g.yAxis").call(d3.axisLeft(this.yAxis));
+
+    if (this.dots) this.svg.selectAll("g.circle").remove();
+    this.dots = this.svg
+      .append("g")
+      .attr("class", "circle")
+      .selectAll("dot")
+      .data(this.data)
+      .enter()
+      .append("circle")
+      .attr("cx", (d: tAdset) => this.xAxis(d.x))
+      .attr("cy", (d: tAdset) => this.yAxis(d.y))
+      .attr("r", 5)
+      .style("fill", (d: tAdset) =>
+        d3.hsl(d.colorHSL.h, d.colorHSL.s, d.colorHSL.l)
+      )
+      .on("mouseover", (d: tAdset) => {
+        this.tooltip
+          .html("x: " + d.x + "<br/>" + "y: " + d.y)
+          .style("top", this.yAxis(d.y) - 36 + "px")
+          .style("left", this.xAxis(d.x) + "px")
+          .style("display", "block")
+          .style("z-index", 3);
+      })
+      .on("mouseout", (d: tAdset) => {
+        this.tooltip.style("display", "none");
+      });
+  }
+
+  private mounted() {
     // svg 생성
     this.svg = d3
       .select("#scatter-plot")
@@ -42,6 +147,7 @@ export default class ScatterPlot extends Vue {
         "translate(" + this.margin.left + "," + this.margin.top + ")"
       );
 
+    // tooltip 생성
     this.tooltip = d3
       .select("#scatter-plot")
       .append("div")
@@ -53,48 +159,8 @@ export default class ScatterPlot extends Vue {
       .style("border-radius", "8px")
       .style("opacity", 0.9);
 
-    // x 축 그리기
-    this.x = d3
-      .scaleLinear()
-      .domain([0, Math.max(...this.data.map(d => d.x))])
-      .range([0, this.width]);
-    this.svg
-      .append("g")
-      .attr("transform", "translate(0, " + this.height + ")")
-      .style("color", "#888")
-      .call(d3.axisBottom(this.x));
-
-    // y축 그리기
-    this.y = d3
-      .scaleLinear()
-      .domain([0, Math.max(...this.data.map(d => d.y))])
-      .range([this.height, 0]);
-    this.svg
-      .append("g")
-      .style("color", "#888")
-      .call(d3.axisLeft(this.y));
-
-    this.svg
-      .append("g")
-      .selectAll("dot")
-      .data(this.data)
-      .enter()
-      .append("circle")
-      .attr("cx", (d: { x: number; y: number }) => this.x(d.x))
-      .attr("cy", (d: { x: number; y: number }) => this.y(d.y))
-      .attr("r", 5)
-      .style("fill", d3.hsl(166, Math.random(), 0.3))
-      .on("mouseover", (d: { x: number; y: number }) => {
-        this.tooltip
-          .html("x: " + d.x + "<br/>" + "y: " + d.y)
-          .style("top", this.y(d.y) - 36 + "px")
-          .style("left", this.x(d.x) + "px")
-          .style("display", "block");
-      })
-      .on("mouseout", (d: { x: number; y: number }) => {
-        this.tooltip.style("display", "none");
-      });
-
+    this.initScatterPlot();
+    this.onMetricChange(this.selectedMetric);
     console.log(d3);
   }
 }
--- src/components/scatterPlot/scatterPlot.vue
+++ src/components/scatterPlot/scatterPlot.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="scatter-plot-wrapper">
     <div class="scatter-plot" id="scatter-plot"></div>
+    <div class="side-bar" ref="sidebar">
+      <div class="color-zone"></div>
+      <div class="button-zone">
+        <a-select
+          :value="selectedMetric[2]"
+          style="width: 100px"
+          @change="changeMetric2"
+        >
+          <a-select-option value="metric1">
+            Metric 1
+          </a-select-option>
+          <a-select-option value="metric2">
+            Metric 2
+          </a-select-option>
+          <a-select-option value="metric3">
+            Metric 3
+          </a-select-option>
+        </a-select>
+      </div>
+    </div>
   </div>
 </template>
 
